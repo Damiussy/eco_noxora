09d8aa36481c1e8c6879bdd46ac54c21b494e44a
Damiussy <dracuglar@gmail.com>
2025-09-16 13:09:48 +0200

    Make logo small and rounded

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function LogoEcoNexora() {
   return (
-    <div className="w-full h-48 rounded-lg overflow-hidden cursor-pointer hover:scale-105 transition-transform duration-300 relative">
+    <div className="size-[100px] rounded-full overflow-hidden relative">
       <Image
         src="/econoxora.png"
         alt="EcoNexora logo"
