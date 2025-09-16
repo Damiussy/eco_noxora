ce0ba76cb3e638887e9f3470020bf81382f25b15
Damiussy <dracuglar@gmail.com>
2025-09-16 11:58:16 +0200

    Update next/image usage in ItineraryMap and LogoEcoNexora components

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 
-export default function ItineraryMap() {
+export default function LogoEcoNexora() {
   return (
     <div className="w-full h-48 rounded-lg overflow-hidden cursor-pointer hover:scale-105 transition-transform duration-300 relative">
       <Image
-        src="/reseau mistral.jpg"
-        alt="Map with Itinerary"
+        src="/econoxora.png"
+        alt="EcoNexora logo"
         fill
         style={{ objectFit: 'cover' }}
       />
